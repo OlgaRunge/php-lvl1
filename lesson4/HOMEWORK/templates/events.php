d888119360fe4acaaa8b2ede2ae5f17358045169
OlgaRunge <angelicaiwf@mail.ru>
2016-06-27 15:59:43 +0300

    lesson4

---
+++ lesson4/HOMEWORK/templates/events.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sekretary
+ * Date: 27.06.2016
+ * Time: 15:44
+ */
---
+++ lesson4/site/www/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once('../config.php');
+renderPage(
+    $_GET['route'],
+    [
+        "id" => $_GET['id']
+    ]
+);
